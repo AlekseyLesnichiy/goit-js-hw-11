e7baef298a3a1d1794788c692a24f8e2611b8dae
AlekseyLesnichiy <91471392+AlekseyLesnichiy@users.noreply.github.com>
2022-02-12 17:11:39 +0200

    ы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './sass/main.scss';
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+{/* <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div> */}
